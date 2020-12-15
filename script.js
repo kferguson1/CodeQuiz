c1b2b64f666be654095d9d6ad49a4f101125fd3e
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2020-12-14 19:05:08 -0500

    Added quiz questions and answers

--- script.js
+++ script.js
@@ -7,3 +7,46 @@ var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
+var quiztimer = document.getElementById("timer");
+
+// Quiz Questions
+var QuizQuestions = [{
+    question: "What does CSS stand for?",
+    answerA: "Computation Style Sharing",
+    answerB: "Commercial Storage String",
+    answerC: "Converstion String Style",
+    answerD: "Cascading Style Sheets",
+    correctAnswer: "d"},
+
+{
+    question: "What does HTML stand for?",
+    answerA: "Hypertext Milenial Loader",
+    answerB: "Hypertext Markup Language",
+    answerC: "Hypertext Mode Language",
+    answerD: "Hypertext Markup Loader",
+    correctAnswer: "b"},
+
+{
+    question: "What does the 'VS' in VS Code stand for?",
+    answerA: "Visual Studio",
+    answerB: "Virtual Studio",
+    answerC: "Visual String",
+    answerD: "Virtual String",
+    correctAnswer: "a"},
+
+{
+    question: "Which of the following is NOT a programming Language?",
+    answerA: "Python",
+    answerB: "Javascript",
+    answerC: "HostGator",
+    answerD: "SQL",
+    correctAnswer: "c"},
+
+{
+    question: "Coding is...",
+    answerA: "Awesome!",
+    answerB: "Fun!",
+    answerC: "The best ever!",
+    answerD: "All of the above",
+    correctAnswer: "d"},
+    ];
